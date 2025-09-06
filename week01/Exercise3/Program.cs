805f7c0cb82a60accac51052d97cc34561980a54
Kris Hernandez <krisalessandrohm@gmail.com>
2025-09-06 16:18:38 -0600

    Exercise 3 finished

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        int guess;
+        Console.Write("Type the Magic number: ");
+        int MagicNumber = int.Parse(Console.ReadLine());
+        do
+        {
+            Console.Write("What is the magic number? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess == MagicNumber)
+            {
+                break;
+            }
+            else if (guess > MagicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("Higher");
+            }
+
+        } while (MagicNumber != guess);
+
+        Console.WriteLine("You guessed it!");
     }
 }
