f3f40db5dc99047aa11ca69037b0a0ef4c94af9c
Kris Hernandez <krisalessandrohm@gmail.com>
2025-09-06 15:35:18 -0600

    added code to the conditionals exercise

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        string letter, text;
+        bool pass = true;
+        
+        Console.Write("Type your grade percentage: ");
+        int grade = int.Parse(Console.ReadLine());
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+            pass = false;
+        }
+
+        if (pass) {
+            text = $"Congratulations! You passed the course with a {letter} grade.";
+        } else {
+            text = $"You got a {letter}. Don't worry, you can do it next time. Keep practicing!";
+        }
+
+
+        Console.WriteLine(text);
+
     }
 }
